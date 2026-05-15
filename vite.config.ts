37e623665e460b84de07a560b131a51dd932633a
Rn-A <rendraa046@gmsil.com>
2026-05-16 00:12:32 +0700

    debug: remove manual chunks to fix script loading

--- vite.config.ts
+++ vite.config.ts
@@ -15,19 +15,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              if (id.includes('recharts')) return 'vendor-recharts';
-              if (id.includes('motion')) return 'vendor-motion';
-              if (id.includes('lucide')) return 'vendor-lucide';
-              if (id.includes('react')) return 'vendor-react-core';
-              return 'vendor-others';
-            }
-          }
-        }
-      },
+      rollupOptions: {}
       chunkSizeWarningLimit: 1000,
       minify: 'esbuild',
     },
